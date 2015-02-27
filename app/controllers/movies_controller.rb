47a25d32f38e9946f3a43722f0f9c03f525f7929
nfowlie <nfowlie@uncc.edu>
2015-02-26 22:15:53 -0400

    4.7 Complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,19 +3,19 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
-end
-# in controller action:
-def show
-  @movie = Movie.find_by_id(params[:id]) # what if this movie not in DB?
-  # BUG: we should check @movie for validity here!
-end
- 
-def new
-  # default: render 'new' template
-end
-# in movies_controller.rb
-def create
-  @movie = Movie.create!(params[:movie])
-  flash[:notice] = "#{@movie.title} was successfully created."
-  redirect_to movies_path
+  # in controller action:
+	def show
+	@movie = Movie.find_by_id(params[:id]) # what if this movie not in DB?
+	# BUG: we should check @movie for validity here!
+	end
+
+	def new
+	# default: render 'new' template
+	end
+	# in movies_controller.rb
+	def create
+	@movie = Movie.create!(params[:movie])
+	flash[:notice] = "#{@movie.title} was successfully created."
+	redirect_to movies_path
+	end
 end
